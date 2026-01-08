28f26675466f1e050aec43dc2f6a7a106ae9fb36
Radu Gheorghiu <radu.gheorghiu@evercomgroup.com>
2026-01-08 19:50:38 +0100

    WIP AI Predictions

--- src/services/ai-prediction-service.ts
+++ src/services/ai-prediction-service.ts
@@ -1,9 +1,9 @@
-import { CreateMLCEngine, InitProgressReport, MLCEngine, prebuiltAppConfig } from "@mlc-ai/web-llm";
+import { ChatCompletionMessageParam, CreateMLCEngine, InitProgressReport, MLCEngine, prebuiltAppConfig } from "@mlc-ai/web-llm";
 import { Predictions, ReviewData } from "../models/types";
 
 export class AIPredictionService {
     private engine: MLCEngine | null = null;
-    private readonly selectedModel = 'Qwen2.5-0.5B-Instruct-q4f16_1-MLC';
+    private readonly selectedModel = 'Llama-3.2-1B-Instruct-q4f32_1-MLC';
     private predictions: Predictions;
     private data: ReviewData;
     constructor(predictions: Predictions, data: ReviewData) {
@@ -20,26 +20,34 @@ export class AIPredictionService {
             );
         }
         try {
-            const systemPrompt = `You are an expert at creating fun and creative titles and subtitle/descriptions for Azure DevOps team achievements based on statistical data.
-            You have to respond only with the title or subtitle/description, without any additional text or explanation.
+            const systemPrompt =
+                `You are an expert at creating funny and creative titles and subtitles/descriptions for Azure DevOps team achievement predictions, based on current or historical statistical data.
+            - The statistical data will be provided in JSON format, but you should NOT mention JSON in your response and DO NOT respond in JSON format, or include ANY JSON in the responses.
+            - DO NOT use markdown formatting.
+            - DO NOT include any or phone numbers.
+            - DO NOT include the text "Prediction Title" or "Subtitle" in the response.
+            - You will be requested for a prediction title only or subtitle only per prompt and have to respond ONLY with the short title OR subtitle/description, without any additional text or explanation.
+            - You can sometimes take creative liberties and ignore strict accuracy for the sake of humor and engagement.
+            - Your prediction text will be used directly in the slides without any modifications.
+            - Avoid repeating the title in the subtitle.
+            - Do not repeat the example.
+
             Example:
-            Title: "More Coffee",
-            Subtitle: "Our AI predicts 20% increase in caffeine consumption."`;
+            Title: More Coffee,
+            Subtitle: Our AI predicts 20% increase in caffeine consumption.`;
             const mainReply = await this.engine.chat.completions.create({
                 messages: [
                     { role: "system", content: systemPrompt },
-                    { role: "user", content: `Based on the following team statistics, provide a creative achievement title and description for the team's performance this year:${JSON.stringify(this.data.stats)}` }
+                    { role: "user", content: `Based on the following team statistics, provide a prediction title for the team's performance this year:${JSON.stringify(this.data.stats)}` }
                 ],
-                temperature: 1.2,
             });
+            const mainContent = mainReply.choices[0].message?.content;
             const subReply = await this.engine.chat.completions.create({
                 messages: [
                     { role: "system", content: systemPrompt },
-                    { role: "user", content: `Based on the following team statistics, provide a creative achievement subtitle/description for the team's performance this year:${JSON.stringify(this.data.stats)}` }
+                    { role: "user", content: `Now create a subtitle/description to go with the prediction title "${mainContent} and the team's performance this year:${JSON.stringify(this.data.stats)}` }
                 ],
-                temperature: 1.2,
             });
-            const mainContent = mainReply.choices[0].message?.content;
             const subContent = subReply.choices[0].message?.content;
             console.log('AI Prediction - Main:', mainContent);
             console.log('AI Prediction - Sub:', subContent);
